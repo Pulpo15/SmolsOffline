3b1c754642f492e91c75b2cdfcb5db9aed5533f6
Pulpo <pol.gomar@enti.cat>
2022-01-19 15:24:14 +0100

    Episode 16 (Bugged)
    Shader doesn't work with URP

--- SmolsOffiline/Assets/Scripts/Procedural Generation/MapGenerator.cs
+++ SmolsOffiline/Assets/Scripts/Procedural Generation/MapGenerator.cs
@@ -124,6 +124,8 @@ public class MapGenerator : MonoBehaviour {
 			}
 		}
 
+		textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+		
 		return new MapData (noiseMap);
 	}
 
